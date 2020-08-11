954b9365a907a0242cab63c394a006c7a65ca21e
William David <williamdavidalmeida36@gmail.com>
2020-08-11 01:08:54 -0400

    Commit Elton Project

---
+++ Projeto_EntreterimentoAlfabeto/Assets/Scritps/TocarSom.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TocarSom : MonoBehaviour
+{
+    public AudioSource somA;
+    
+    void TocarA(){
+        somA.Play();
+    }
+    
+    void TocarB(){
+        somA.Pause();
+    }
+}
