7d0e9a7a3d0eaee63622126cb23e3fd32f668015
William David <williamdavidalmeida36@gmail.com>
2020-08-14 00:48:51 -0400

    Add Tela Inicial

---
+++ Projeto_EntreterimentoAlfabeto/Assets/Scritps/LoadScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+    // Cenas
+
+public class LoadScene : MonoBehaviour
+{
+   public void CarregarCena(string nomeCena){
+       SceneManager.LoadScene(nomeCena);
+   } 
+
+   
+}
